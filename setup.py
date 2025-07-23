22d82aa3384020e8b0a2efa7c22d57f9d3ab625d
Emilio Dulay <futuredrdulay@gmail.com>
2025-07-23 16:26:52 -0700

    update 0.5, added food_ids.csv to the package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='my_datasets_lib',
-    version='0.4',
+    version='0.5',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
