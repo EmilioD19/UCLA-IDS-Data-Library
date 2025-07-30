e15a62a0e778678fb84d38604254ab147f65ddd3
Emilio Dulay <futuredrdulay@gmail.com>
2025-07-30 14:10:30 -0700

    new update

--- my_datasets_lib/datasets/__init__.py
+++ my_datasets_lib/datasets/__init__.py
@@ -1,5 +1,5 @@
 '''Makes datasets behave like bundles of submodules.'''
 
-from . import cdc
+from . import cdc, food_ids, atu_dirty
 
-__all__ = ["cdc", "food_ids"]
+__all__ = ["cdc", "food_ids", "atu_dirty"]
--- my_datasets_lib/datasets/food_ids.py
+++ my_datasets_lib/datasets/food_ids.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 def load():
-    """Load the cdc dataset."""
+    """Load the food dataset."""
     path = os.path.join(os.path.dirname(__file__), '..', 'data', 'food_ids.csv')
     df = pd.read_csv(path)
     return df
